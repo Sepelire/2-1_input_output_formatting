a7f531b9a853df5ad76f4c239a9ae42e8204bec9
Sepelire <arthurpan2001@mail.ru>
2025-01-17 02:17:50 +0500

    Complete: change.py

---
+++ change.py
@@ -0,0 +1,7 @@
+print('\nEXAMPLE:\nDenomination of the banknote(>94): 100\n5\n')
+
+cherries_in_kg = float(2.5)
+price_per_kg = float(38)
+bill_of_buyer = float(input('Denomination of the banknote: '))
+
+print(int(bill_of_buyer - cherries_in_kg * price_per_kg))
