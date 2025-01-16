3904d1c634f44d3d35209cc7c739d133dbbd56b8
Sepelire <arthurpan2001@mail.ru>
2025-01-16 21:17:52 +0500

    Complete: over_automation.py

---
+++ over_automation.py
@@ -0,0 +1,4 @@
+print(f'\nEXAMPLE:\nEnter a string: 2 + 2 = 4\nResult:\n2 + 2 = 4\n2 + 2 = 4\n2 + 2 = 4\n')
+string = str(input(f'Enter a string: ')) + '\n'
+
+print(f'Result:\n{string * 3}', end='')
